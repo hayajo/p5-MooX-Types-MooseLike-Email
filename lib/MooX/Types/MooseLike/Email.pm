05a02d9c43a86de4f72a3396117b15ace9c8745f
hayajo <hayajo@cpan.org>
2013-01-19 09:20:03 +0900

    Checking in changes prior to tagging of version 0.03.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index 8e41cb7..9b65bf0 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,8 @@
     Revision history for Perl extension MooX::Types::MooseLike::Email
    
    +0.03
    +        - Fixed a bug #82807
    +
     0.02
             - Modified a dependance of the MooX::Types::MooseLike

--- lib/MooX/Types/MooseLike/Email.pm
+++ lib/MooX/Types/MooseLike/Email.pm
@@ -1,7 +1,7 @@
 package MooX::Types::MooseLike::Email;
 use strict;
 use warnings;
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 use MooX::Types::MooseLike qw/exception_message/;
 use MooX::Types::MooseLike::Base;
